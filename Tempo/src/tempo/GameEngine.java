ce0e83512cdf0455ea6b88b8c248deef0cda7d04
Frupz <eric.vickstrom98@gmail.com>
2016-10-21 22:56:13 +0200

    La in ticks och frames.

--- Tempo/src/tempo/GameEngine.java
+++ Tempo/src/tempo/GameEngine.java
@@ -5,10 +5,74 @@ package tempo;
  *
  * @author Oscar Almqvist
  */
-public class GameEngine {
+public class GameEngine implements Runnable {
     GamePanel gp;
+    
+    public boolean running = false;
     
     public GameEngine(GamePanel gp){
         this.gp = gp;
+        start();
     }
+    
+    public synchronized void start() {
+        running = true;
+        new Thread(this).start();
+    }
+    
+    
+    public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D/60D;
+        
+        int ticks = 0;
+        int frames = 0;
+        
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0;
+        
+        
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+            
+            
+            try {
+                Thread.sleep(2);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta -= 1;
+                shouldRender = true;
+            }
+            
+            if(shouldRender) {
+                frames++;
+                render();
+            }
+            
+            if(System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println(ticks + "ticks, " + frames + " frames");
+                frames = 0;
+                ticks = 0;
+            }
+            
+        }
+    }
+    
+        public void tick() {
+            gp.player.x += 10;
+        }
+        
+        public void render() {
+            gp.repaint();
+        }
+    
 }
