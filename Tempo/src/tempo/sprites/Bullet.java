c247985a38bf9ec447adbe16845f8d8e6a9ade1a
PierreSegerstrom <pierre.segerstrom@gmail.com>
2016-10-28 14:37:29 +0200

    Gravity på bullet

--- Tempo/src/tempo/sprites/Bullet.java
+++ Tempo/src/tempo/sprites/Bullet.java
@@ -6,18 +6,20 @@ import java.awt.image.BufferedImage;
 
 public class Bullet extends Sprites {
     
-    public float angle;
+    public double rads;
     public float speed = 10;
+    public float ySpeed = 0;
+    public double gravity = 0.5;
     public boolean killBullet = false;
     
     
     public Bullet(int x, int y, int width, int height, float angle, BufferedImage image) {
         super(x, y, width, height, image);
-        this.angle = angle;
+        this.rads = angle*(Math.PI/180);
+        this.ySpeed = (float)(Math.sin(rads) * speed);
     }
     
     public void shoot() {
-        double rads = angle*(Math.PI/180);
         super.x += Math.cos(rads) * speed;
         super.y += Math.sin(rads) * speed;
         if(x < 0 || x > 1200) {
