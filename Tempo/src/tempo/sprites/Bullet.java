424bcf790a5e0d9a3cea18e86d71120e35068b53
Eric Vickström <Elev@10.100.11.22>
2016-10-28 10:11:51 +0200

    Dödar skott efter den går ut ur skärmen

--- Tempo/src/tempo/sprites/Bullet.java
+++ Tempo/src/tempo/sprites/Bullet.java
@@ -8,6 +8,7 @@ public class Bullet extends Sprites {
     
     public float angle;
     public float speed = 10;
+    public boolean killBullet = false;
     
     
     public Bullet(int x, int y, int width, int height, float angle, BufferedImage image) {
@@ -19,6 +20,9 @@ public class Bullet extends Sprites {
         double rads = angle*(Math.PI/180);
         super.x += Math.cos(rads) * speed;
         super.y += Math.sin(rads) * speed;
+        if(x < 0 || x > 1200) {
+            killBullet = true;
+        }
     }
     
     @Override
