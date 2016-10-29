f567b335f72c815b489179de8fda73348c642542
oscaralmqvist <almqvist.oscar@gmail.com>
2016-10-29 22:00:57 +0200

    ändrade så att float angle går från handen

--- Tempo/src/tempo/Mouse.java
+++ Tempo/src/tempo/Mouse.java
@@ -32,7 +32,7 @@ public class Mouse implements MouseListener {
     public void mousePressed(MouseEvent e) {
         if(!gp.isPaused){
             if(gp.enemy.nuts >0 && !gp.enemy.reloading){
-                float angle = (float) Math.toDegrees(Math.atan2(e.getY() - gp.enemy.y, e.getX() - gp.enemy.x));
+                float angle = (float) Math.toDegrees(Math.atan2(e.getY() - (gp.enemy.y + gp.enemy.height/2), e.getX() - (gp.enemy.x + gp.enemy.currentHand)));
                 int i = (int)(Math.random() * 4 + 30);
                 gp.bullets.add(new Bullet(gp.enemy.x + gp.enemy.currentHand, gp.enemy.y + gp.enemy.height/2, i, i, angle, gp.ss.getSprite(2, 2, 1, 1)));
                 gp.enemy.nuts--;
