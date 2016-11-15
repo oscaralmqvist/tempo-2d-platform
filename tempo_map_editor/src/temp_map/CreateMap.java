eebd69a60b21d38fad533f4fe4ad2900afb641ec
Eric Vickström <Elev@10.100.8.175>
2016-11-15 17:17:09 +0100

    Lättare att lägga in blocks.

--- tempo_map_editor/src/temp_map/CreateMap.java
+++ tempo_map_editor/src/temp_map/CreateMap.java
@@ -7,57 +7,32 @@ import java.util.ArrayList;
 public class CreateMap {
     
 
-    public CreateMap(ArrayList<Block> bs, int blockSize, String path) {
+    public CreateMap(ArrayList<Block> bs, ArrayList<BlockData> blocksdata, int blockSize, String path) {
 
         try{
             PrintWriter writer = new PrintWriter(path + ".txt", "UTF-8");
-            /*
-            String line = "";
-            int currentline = 0;
-            for(int i = 0; i < bs.size(); i++) {
-                if(bs.get(i).y != bs.get(i+1).y) {
-                    System.out.println(line);
-                    writer.println("asdasd");
-                    System.out.println(currentline);
-                    currentline++;
-                    line = "";                    
-                } else {
-                    if(bs.get(i).id == 0) {
-                        line += "-";
-                    }
-                    if(bs.get(i).id == 1) {
-                        line += "l";
-                    }  
-                }
-            }
-            */
-            
             String line = "";
             int currentline = 0;
            
             for(Block b : bs) {
                 if(currentline == (b.y / blockSize)) {
-                    if(b.id == 0) {
-                        line += "-";
+                    for(BlockData bd : blocksdata) {
+                        if(b.id == bd.getID())
+                            line += bd.getCharID();
                     }
-                    if(b.id == 1) {
-                        line += "l";
-                    }     
-                    if(b.id == 2) {
-                        line += "d";
+                    if(b.id == 0) {
+                        line += '-';
                     }
                 } else {
                     System.out.println(currentline);
                     writer.println(line);
                     System.out.println(bs.size());
+                    for(BlockData bd : blocksdata) {
+                        if(b.id == bd.getID())
+                            line = "" + bd.getCharID();
+                    }
                     if(b.id == 0) {
                         line = "-";
-                    }
-                    if(b.id == 1) {
-                        line = "l";
-                    }   
-                    if(b.id == 2) {
-                        line += "d";
                     }
                     currentline++;
                 }
