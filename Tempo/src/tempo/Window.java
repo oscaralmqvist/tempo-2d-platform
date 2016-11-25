46d2e2d015cbad12993e85074df560524af9c1bb
MaxNorling <max.norling@gmail.com>
2016-11-25 10:18:22 +0100

    Spelat pausas när man tabbar ut

--- Tempo/src/tempo/Window.java
+++ Tempo/src/tempo/Window.java
@@ -27,5 +27,43 @@ public class Window extends JFrame{
         addKeyListener(new Keyboard(panel));
         addMouseListener(new Mouse(panel));
     //    setLocationRelativeTo(null);
+    
+        this.addWindowListener(new WindowAdapter() {
+      public void windowOpened(WindowEvent e) {
+      }
+
+      public void windowClosing(WindowEvent e) {
+      }
+
+      public void windowClosed(WindowEvent e) {
+      }
+
+      public void windowIconified(WindowEvent e) {
+      }
+
+      public void windowDeiconified(WindowEvent e) {
+      }
+
+      public void windowActivated(WindowEvent e) {
+        panel.isPaused = false;
+
+      }
+
+      public void windowDeactivated(WindowEvent e) {
+        panel.isPaused = true;
+
+      }
+
+      public void windowStateChanged(WindowEvent e) {
+      }
+
+      public void windowGainedFocus(WindowEvent e) {
+      }
+
+      public void windowLostFocus(WindowEvent e) {
+      }
+    });
+        
     }
 }
+                
