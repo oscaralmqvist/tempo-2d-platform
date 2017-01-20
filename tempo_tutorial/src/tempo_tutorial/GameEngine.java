248a694016fc37e063f57d21f8b0586efb3fdd1e
oscaralmqvist <almqvist.oscar@gmail.com>
2017-01-20 17:13:44 +0100

    Fyllde GameEngine med gamla tick systemet.

--- tempo_tutorial/src/tempo_tutorial/GameEngine.java
+++ tempo_tutorial/src/tempo_tutorial/GameEngine.java
@@ -1,12 +1,74 @@
 
 package tempo_tutorial;
 
-public class GameEngine {
+public class GameEngine implements Runnable{
     
     GamePanel gp;
     
+    private boolean running = false;
+    
     public GameEngine(GamePanel gp) {
         this.gp = gp;
+        start();
     }
     
+    public synchronized void start() {
+        running = true;
+        new Thread(this).start();
+    }
+    
+    
+    public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D/60D;
+        
+        int ticks = 0;
+        int frames = 0;
+        
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0;
+        
+        
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+            
+            
+            try {
+                Thread.sleep(2);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta -= 1;
+                shouldRender = true;
+            }
+            
+            if(shouldRender) {
+                frames++;
+                render();
+            }
+            
+            if(System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println(ticks + "ticks, " + frames + " frames");
+                frames = 0;
+                ticks = 0;
+            }
+            
+        }
+    }
+    
+    public void tick() {
+        
+    }
+        
+    public void render() {
+        gp.repaint();
+    }
 }
