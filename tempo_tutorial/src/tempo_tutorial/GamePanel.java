9cbcf1cae395eff4da59ab567cd8036009dc7ba7
Eric Vickström <Elev@elevdatrht20142.sigtuna.local>
2017-01-20 08:59:12 +0100

    Uppdaterar..

--- tempo_tutorial/src/tempo_tutorial/GamePanel.java
+++ tempo_tutorial/src/tempo_tutorial/GamePanel.java
@@ -1,11 +1,20 @@
 
 package tempo_tutorial;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
     
+    
     public GamePanel() {
+        
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         
     }
 
