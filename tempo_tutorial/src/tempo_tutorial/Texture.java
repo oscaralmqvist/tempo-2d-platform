4ccc6ba9a1bbad6da871f38fd5572546a3eb03cd
pedagog <pedagog@mac15561.sigtuna.local>
2017-01-26 10:20:00 +0100

    Lagt till kommentarer och metodförklaringar

--- tempo_tutorial/src/tempo_tutorial/Texture.java
+++ tempo_tutorial/src/tempo_tutorial/Texture.java
@@ -7,7 +7,12 @@ import javax.imageio.ImageIO;
 
 public class Texture {
     
+    //Variabeln "sheet" kommer vara bilden vi hämtar alla sprites ifrån
     BufferedImage sheet = null;
+    
+    /**
+     * Läser in spritesheet
+     */
     
     public Texture(){
         try {
@@ -15,21 +20,41 @@ public class Texture {
         } catch(Exception e) {}
     }
     
+    /**
+     * Hämtar del utav spritesheet utifrån givna värden
+     * @param row antal rader
+     * @param column antal kolumner
+     * @param height höjd i kolumner
+     * @param width höjd i rader
+     * @return Sprite (bild)
+     */
+    
     public BufferedImage getSprite(int row, int column, int height, int width){
-        // längden på en rad/kolumn i spritesheet i pixlar
+        // Längden på en rad/kolumn i spritesheet i pixlar
         int a = 32;
         try {
+            //Hämtar sprite
             return sheet.getSubimage(row*a, column*a, height*a, width*a);
         } catch(Exception e) {
+            //Hämtar error-sprite om värden går utanför ss storlek
             return sheet.getSubimage(3*a, 0, a, a);
         }
     }
     
+    /**
+     * Läser in en bild baserat på sökväg
+     * @param content sökväg
+     * @return Bild
+     */
+    
     public BufferedImage getImage(String content){
+        //Deklarerar variabel
         BufferedImage tempImage = null;
         try {
+            //Letar efter efterfrågad fil
             tempImage = ImageIO.read(new File(content));
         } catch(Exception e) {
+            //Om filen inte hittas, skrivs error-meddelande
             System.out.println(content + "-filen kunde inte hittas");
         }
         return tempImage;
