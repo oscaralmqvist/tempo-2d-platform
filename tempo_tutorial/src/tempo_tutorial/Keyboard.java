30bcc878d8ee258f1c721c7489d665013e00c214
MaxNorling <max.norling@gmail.com>
2017-01-27 15:06:24 +0100

    Keyboard

--- tempo_tutorial/src/tempo_tutorial/Keyboard.java
+++ tempo_tutorial/src/tempo_tutorial/Keyboard.java
@@ -16,9 +16,36 @@ public class Keyboard implements KeyListener{
     public void keyTyped(KeyEvent e) {}
 
     @Override
-    public void keyPressed(KeyEvent e) {}
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_D:
+                //gp.getPlayer().getRectangle().x = (int)(gp.getPlayer().getRectangle().x+gp.getPlayer().getVelocityX());
+                if(!gp.getPlayer().getMovingLeft()){
+                    gp.getPlayer().setMovingLeft(true);
+                }
+            break;
+                
+            case KeyEvent.VK_A:
+               //gp.getPlayer().getRectangle().x = (int)(gp.getPlayer().getRectangle().x-gp.getPlayer().getVelocityX());
+                if(!gp.getPlayer().getMovingRight()){
+                    gp.getPlayer().setMovingRight(true);
+                }
+            break;
+     
+        }
+    }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+        switch(e.getKeyCode()){
+                case KeyEvent.VK_A:
+                    gp.getPlayer().setMovingRight(false);
+                break;
+                
+                case KeyEvent.VK_D:
+                    gp.getPlayer().setMovingLeft(false);
+                break;
+        }
+    }
     
 }
--- tempo_tutorial/src/tempo_tutorial/sprite/Player.java
+++ tempo_tutorial/src/tempo_tutorial/sprite/Player.java
@@ -7,6 +7,8 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Player extends Sprite {
+        private boolean movingLeft, movingRight;
+
     
     public Player(int x, int y, int width, int height, BufferedImage image, float dx, float dy, float gravity) {
         super(x, y, width, height, image, dx, dy, gravity);
@@ -17,10 +19,31 @@ public class Player extends Sprite {
         Rectangle r = super.getRectangle();
         g.drawImage(super.getImage(), r.x, r.y, r.width, r.height, null);
     }
+
     
     public void tick() {
         super.getRectangle().y += super.getVelocityY();
         super.addVelocityY(super.getGravity());
+        
+        if(movingLeft){
+           getRectangle().x += getVelocityX();
+        }
+        if(movingRight){
+           getRectangle().x -= getVelocityX();
+        }
     }
     
+    public boolean getMovingLeft(){
+        return movingLeft;
+    }
+    public boolean getMovingRight(){
+        return movingRight;
+    }
+    public void setMovingLeft(boolean b){
+        movingLeft = b;
+    }
+    public void setMovingRight(boolean b){
+        movingRight = b;
+    }
+        
 }
