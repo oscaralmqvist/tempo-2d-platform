16f95985854ed1cbdfd40dd29822c04d755b88ee
Oscar <almqvist.oscar@gmail.com>
2017-02-17 09:17:34 +0100

    Kommenterade Player-klassen

--- tempo_tutorial/src/tempo_tutorial/sprite/Player.java
+++ tempo_tutorial/src/tempo_tutorial/sprite/Player.java
@@ -6,24 +6,37 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import tempo_tutorial.Animation;
 
+/**
+ * Player extendar Sprite
+ * @author Elev
+ */
 public class Player extends Sprite {
-    private boolean movingLeft, movingRight, jumping, falling, flipped;
-    private Animation animation;
-    private int jumps = 1;
+        private boolean movingLeft, movingRight, jumping, falling, flipped;
+        private Animation animation;
+        private int jumps = 1;
 
-    
     public Player(int x, int y, int width, int height, BufferedImage image, float dx, float dy, float gravity) {
         super(x, y, width, height, image, dx, dy, gravity);
     }
 
+    /**
+     * Ritar ut Playerns bild beroende på vilket håll bilden ska ritas ut på
+     * @param g Graphics
+     */
     @Override
     public void paint(Graphics2D g) {
         Rectangle r = super.getRectangle();
-        if (flipped) g.drawImage(super.getImage(), r.width + r.x, r.y, -r.width, r.height, null);
-        else g.drawImage(super.getImage(), r.x, r.y, r.width, r.height, null);
+        if (flipped) 
+            g.drawImage(super.getImage(), r.width + r.x, r.y, -r.width, r.height, null);
+        else 
+            g.drawImage(super.getImage(), r.x, r.y, r.width, r.height, null);
     }
 
-    
+
+    /**
+     * Bestämmer hur x och y positioner ändras varje tick
+     * samt om Playern rör sig åt vänster eller höger
+     */
     public void tick() {
         super.getRectangle().y += super.getVelocityY();
         super.addVelocityY(super.getGravity());
