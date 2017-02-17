d45e429abb09de3ce95a8afd3cdcc56ddae38646
Eric Vickström <Elev@10.100.11.171>
2017-02-17 09:19:35 +0100

    Ändrade main-class till Tempo

---
+++ tempo_tutorial/src/tempo_tutorial/Tempo.java
@@ -0,0 +1,17 @@
+
+package tempo_tutorial;
+
+public class Tempo {
+
+    public static String NAME = "Tempo";
+    public static int WIDTH = 1280;
+    public static int HEIGHT = 768;
+    
+    public Tempo() {
+        new Window();
+    }
+    
+    public static void main(String[] args) {
+        new Tempo();
+    }
+}
