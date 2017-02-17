5add6903d0c21ce3c99651cf2700f110459af434
Oscar <almqvist.oscar@gmail.com>
2017-02-17 09:15:27 +0100

    Kommenterade Sprite-klassen

--- tempo_tutorial/src/tempo_tutorial/sprite/Sprite.java
+++ tempo_tutorial/src/tempo_tutorial/sprite/Sprite.java
@@ -5,12 +5,27 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
+/**
+ * Sprites är alla objekt som ritas ut på skärmen.
+ * Här finns alla värden en Sprite kan ha
+ * @author Elev
+ */
 public abstract class Sprite {
     
     private Rectangle rect;
     private BufferedImage image;
     private float dx, dy, gravity;
     
+    /**
+     * @param x Spritens x-position
+     * @param y Spritens y-position
+     * @param width Spritens-bredd
+     * @param height Spritens-längd
+     * @param image Spritens bild
+     * @param dx Spritens hastigheten i x-led
+     * @param dy Spritens hastigheten i y-led
+     * @param gravity Spritens gravitation
+     */
     public Sprite(int x, int y, int width, int height, BufferedImage image, float dx, float dy, float gravity) {
         this.rect = new Rectangle(x, y, width, height);
         this.image = image;
